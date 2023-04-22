3ea2e2f0d7cf1a5dd531620cacfebc506ff8187a
Tiago Costa <tiagodpv@gmail.com>
2023-04-22 00:33:46 -0400

    add routes

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const { randomUUID } = require('crypto');
 
 const app = express();
+
+app.use(express.json());
 
 const products = [];
 
@@ -19,9 +22,47 @@ const products = [];
 
 app.post('/products', (request, response) => {
   //Nome e preço
-  const body = request.body;
+  const { name, price } = request.body;
 
-  console.log(body);
+  const product = { name, price, id: randomUUID() };
+
+  products.push(product);
+
+  return response.json(product);
+});
+
+app.get('/products', (request, response) => {
+  return response.json(products);
+});
+
+app.get('/products/:id', (request, response) => {
+  const { id } = request.params;
+  const product = producs.find((product) => product.id === id);
+
+  return response.json(product);
+});
+
+app.put('/products/:id', (request, response) => {
+  const { id } = request.params;
+  const { name, price } = request.body;
+
+  const productIndex = products.findIndex((product) => product.id === id);
+  products[productIndex] = {
+    ...products[productIndex],
+    name,
+    price,
+  };
+
+  return response.json({ message: 'produto alterado com sucesso' });
+});
+
+app.delete('/products/:id', (request, response) => {
+  const { id } = request.params;
+  const productIndex = products.findIndex((product) => product.id === id);
+
+  products.splice(productIndex, 1);
+
+  return response.json({ message: 'produto removido com sucesso' });
 });
 
 app.listen(4002, () => console.log('Servidor esta rodando na porta 4002'));
